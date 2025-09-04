89840909b1b999ef15419ce3aed62ee286eaaf93
Nagaraju <nagarajucivillbrce123@gmail.com>
2025-09-04 18:30:56 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "resource" {
   ami = "ami-0de716d6197524dd9"
-  instance_type ="t2.nano" 
+  instance_type ="t2.medium" 
   availability_zone = "us-east-1d"
   tags = {
     Name = "raja"
